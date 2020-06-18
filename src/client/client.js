cbb53c426585e22ac6fbfba694b20d5803bb2ab1
hsimao <e087754958@gmail.com>
2020-06-18 20:29:01 +0800

    feat: setting client axios instance

--- src/client/client.js
+++ src/client/client.js
@@ -7,11 +7,16 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
 import { renderRoutes } from 'react-router-config'
+import axios from 'axios'
 import Routes from './Routes'
 import reducers from './reducers'
 
+const axiosInstance = axios.create({
+  baseURL: '/api',
+})
+
 // 將 server 保存的狀態注入 store 內
-const store = createStore(reducers, window.INITIAL_STATE, applyMiddleware(thunk))
+const store = createStore(reducers, window.INITIAL_STATE, applyMiddleware(thunk.withExtraArgument(axiosInstance)))
 
 ReactDOM.hydrate(
   <Provider store={store}>
